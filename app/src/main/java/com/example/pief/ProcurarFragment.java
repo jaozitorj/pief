760283f6c7bd6c171c9cec004e4592c2a1580132
jaozitorj <107441835+jaozitorj@users.noreply.github.com>
2022-08-29 16:56:19 -0300

    10

--- app/src/main/java/com/example/pief/ProcurarFragment.java
+++ app/src/main/java/com/example/pief/ProcurarFragment.java
@@ -1,27 +1,56 @@
 package com.example.pief;
 
-import android.content.ClipData;
+
 import android.os.Bundle;
 
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class ProcurarFragment extends Fragment {
+    SearchView searchView;
+    ListView listView;
+    ArrayList arrayList;
+    ArrayAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_procurar, container, false);
+        searchView = view.findViewById(R.id.procurar);
+        listView = view.findViewById(R.id.listview);
 
+        arrayList = new ArrayList<>();
+
+        arrayList.add("Paulo Henrique");
+        arrayList.add("Gabriel Camargo");
+
+
+        adapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
 
         return view;
     }
